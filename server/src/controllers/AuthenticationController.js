02b1d5a5cf3f38ef16b5e01cd7dc6c32620cf79b
davemflick <davemflick@yahoo.com>
2018-05-22 16:59:08 -0400

    tokens

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -1,5 +1,13 @@
 const {User} = require('../models');
+const jwt = require("jsonwebtoken");
+const config = require('../config/config');
 
+function jwtSignUser(user){
+	const ONE_WEEK = 60 * 60 * 24 * 7;
+	return jwt.sign(user, config.authentication.jwtSecret, {
+		expiresIn: ONE_WEEK
+	})
+}
 module.exports = {
 	async register(req, res){
 		try{
@@ -37,7 +45,8 @@ module.exports = {
 			const userJson = user.toJSON();
 
 			res.send({
-				user: userJson
+				user: userJson,
+				token: jwtSignUser(userJson)
 			});
 
 		} catch(err){
