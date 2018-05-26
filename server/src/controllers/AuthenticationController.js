e5e1bc0708baf0feebdca82f39952b58e60232f2
davemflick <davemflick@yahoo.com>
2018-05-26 08:37:44 -0400

    hashing passwords

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -34,7 +34,7 @@ module.exports = {
 				})
 			}
 			
-			const isPasswordValid = password === user.password;
+			const isPasswordValid = await user.comparePassword(password);
 
 			if(!isPasswordValid){
 				res.status(403).send({
