d64ebf194a18860d238813e5f8c5dbfcb90f9f2d
davemflick <davemflick@yahoo.com>
2018-05-27 09:18:02 -0400

    Songs component added

---
+++ client/src/components/Songs.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-layout row>
+    <v-flex xs6 offset-xs3>
+      <panel title="Songs">
+        <p>Songs slot</p>
+      </panel>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Panel from './Panel.vue';
+
+export default{
+  components: {
+    Panel
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
