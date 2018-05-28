0f9fa5b474f4f63bfbf3fa725f86de4bea137dc6
davemflick <davemflick@yahoo.com>
2018-05-28 07:48:52 -0400

    styling and link

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -5,12 +5,20 @@
           <v-btn class="cyan accent-2" fab light small absolute right middle slot="action" @click="navigateTo({name: 'songs-create' })">
             <v-icon>add</v-icon>
           </v-btn>
-        <div v-for="song in songs" :key="song.id">
-          <h5>{{ song.title }}</h5>
-          <p>
-            By: {{ song.artist }} <br />
-            Album: {{ song.album }}
-          </p>
+        <div class="song" v-for="song in songs" :key="song.id">
+          <v-layout>
+            <v-flex xs6 mx-1>
+              <div class="song-title">{{song.title}}</div>
+              <div class="song-artist">{{song.artist}}</div>
+              <div class="song-genre">{{song.genre}}</div>
+              <v-btn dark class="cyan" @click="navigateTo({name: 'song', params: {songId: song.id}})">
+                View
+              </v-btn>
+            </v-flex>
+            <v-flex xs6 mx-1>
+              <img class="album-image" :src="song.albumImageUrl" />
+            </v-flex>
+          </v-layout>
         </div>
       </panel>
     </v-flex>
@@ -43,5 +51,22 @@
 </script>
 
 <style scoped>
-
+  .song{
+    padding: 20px;
+    height: 330px;
+    overflow: hidden;
+  }
+  .song-title{
+    font-size: 28px;
+  }
+  .song-artist{
+    font-size: 24px;
+  }
+  .song-genre{
+    font-size: 18px;
+  }
+  .album-image{
+    max-width: 100%;
+    margin: 0 auto;
+  }
 </style>
