e259f32fde27e73e9c86bd90ee80eeebddde5612
davemflick <davemflick@yahoo.com>
2018-05-29 16:57:43 -0400

    bookmarks table

--- client/src/components/songs/Index.vue
+++ client/src/components/songs/Index.vue
@@ -1,6 +1,11 @@
 <template>
   <v-layout row wrap>
-    <v-flex xs6 offset-xs3 my2>
+    <v-flex  xs6 offset-xs3 my-2>
+      <songs-bookmarks>
+
+      </songs-bookmarks>
+    </v-flex>
+    <v-flex xs6 offset-xs3 my-2>
      <songs-search-panel></songs-search-panel>
    </v-flex>
     <v-flex xs6 offset-xs3 my-2>
@@ -11,6 +16,7 @@
 
 <script>
   import SongsPanel from './SongsPanel.vue';
+  import SongsBookmarks from './SongsBookmarks.vue';
   import SongsSearchPanel from './SongsSearchPanel.vue';
   import Panel from '../Panel.vue';
   import SongService from '@/services/SongService.js';
@@ -18,7 +24,8 @@
     components: {
       Panel,
       SongsPanel,
-      SongsSearchPanel
+      SongsSearchPanel,
+      SongsBookmarks
     }
   }
 
---
+++ client/src/components/songs/SongsBookmarks.vue
@@ -0,0 +1,56 @@
+<template>
+  <panel title="Bookmarks" my2>
+    <v-data-table :headers="headers" :pagination.sync="pagination" :items="bookmarks">
+      <template slot="items" scope="props">
+        <td class="text-xs-right">
+          {{props.item.title}}
+        </td>
+        <td class="text-xs-right">
+          {{props.item.artist}}
+        </td>
+      </template>
+    </v-data-table>
+  </panel>
+</template>
+
+<script>
+  import Panel from '../Panel.vue';
+  export default{
+    data(){
+      return {
+        headers: [
+        {
+          text: 'Title',
+          value: 'title'
+        },
+        {
+          text: 'Artist',
+          value: 'artist'
+        }
+        ],
+        pagination: {sortBy: 'date', descending: true},
+        bookmarks: [
+        {
+          title: 'Farts',
+          artist: 'Testing'
+        },
+        {
+          title: 'Hello Wolrd2',
+          artist: 'Testing2'
+        }
+        ]
+      }
+    },
+    async mounted(){
+
+    },
+    components:{
+      Panel
+    }
+  }
+
+</script>
+
+<style scoped>
+
+</style>
